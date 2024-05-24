501aa865834cd6a4c8a986109b6772cbeeadc972
cuijian <cuijian@zuzuche.com>
2024-05-24 15:26:16 +0800

    feat: add docker mirrors and install docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,10 +31,18 @@ swapoff -a
 #  添加Google的GPG密钥
 curl -s https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
  
-#  添加Kubernetes的APT仓库
+# 添加Kubernetes的APT仓库
 cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
 
+# 添加阿里云 Docker 的 GPG 密钥
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# 添加 docker 的阿里云源
+cat <<EOF | tee /etc/apt/sources.list.d/docker.list
+deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable
+EOF
+
 # 安装 Docker
-apt update && apt install -y kubelet kubeadm kubectl
+apt update && apt install -y docker-ce docker-ce-cli containerd.io kubelet kubeadm kubectl
