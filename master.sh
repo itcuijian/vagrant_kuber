b856a0e1556bb15f1c92b2103efa74726fb91b4d
cuijian <cuijian@zuzuche.com>
2024-10-29 09:48:26 +0800

    feat: add kubernetes-dashboard install config

--- master.sh
+++ master.sh
@@ -11,11 +11,18 @@ kubectl apply -f /vagrant/examples/flannel/kube-flannel.yml
 # 安装面板工具
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-# 创建永久 token
-kubectl apply -f dashboard/dashboard-user-token.yaml
-# 启动
-# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
+helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard -n kubernetes-dashboard --create-namespace -f examples/dashboard/values.yaml
+# 创建 Admin 用户
+kubectl apply -f examples/dashboard/dashboard-user.yaml
+kubectl apply -f examples/dashboard/dashboard-user-token.yaml
+# 获取 token
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+# 获取访问端口
+kubectl -n kubernetes-dashboard get svc | grep kubernetes-dashboard-kong-proxy
+# 访问
+# https://10.0.0.30:22762
+
+# 安装Metrics Server
 
 # 存储插件
 # sudo apt install ceph-common
